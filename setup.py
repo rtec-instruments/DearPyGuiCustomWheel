0312b81b6768a3fe98bf0ad65ca086926c5bd6dd
Thomas Bougeard <thomas.bougeard@epitech.eu>
2025-03-17 10:06:54 +0100

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='dearpygui_special_wheel',
+    version='1.11.0',
+    description='Custom DearPyGui wheel',
+    packages=['dearpygui_special_wheel'],
+    include_package_data=True,
+)
