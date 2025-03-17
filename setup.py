78f5a1e0c4470df0b3a080e2d497f948f81d7999
Thomas Bougeard <thomas.bougeard@epitech.eu>
2025-03-17 10:08:18 +0100

    feat: rm setup

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='dearpygui_special_wheel',
-    version='1.11.0',
-    description='Custom DearPyGui wheel',
-    packages=['dearpygui_special_wheel'],
-    include_package_data=True,
-)
